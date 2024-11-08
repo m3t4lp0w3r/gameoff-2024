63f34ddba1b6afd00fc6958bb4c8baf99499113e
Guidi Simone <simone.guidi@tutanota.com>
2024-11-08 07:48:44 +0100

    + use invetory system

--- levels/test_simone/scripts/item_test.gd
+++ levels/test_simone/scripts/item_test.gd
@@ -1,15 +1,10 @@
 extends StaticBody2D
 
-@onready var sprite = $Sprite2D2
+@export var item : ItemData
 
 var color = 0;
 
 func _on_interactable_interacted() -> void:
 	print("player interaction with", name)
-	if color == 0:
-		sprite.modulate = Color.RED
-		color = 1
-	elif color == 1:
-		sprite.modulate = Color.BLUE
-		color = 0
-	pass # Replace with function body.
+	InventorySystem.pick_up(item)
+	hide()
