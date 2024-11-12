6c4c4d88cee54f88b61cfb738b6f7f5944bd9323
Guidi Simone <simone.guidi@tutanota.com>
2024-11-12 18:57:17 +0100

    + hide npc after the player talked to them

--- levels/test_samanta/scripts/other_level.gd
+++ levels/test_samanta/scripts/other_level.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == %Player:
+		LevelSystem.call_deferred("load_level", "Test Samy")
+	pass # Replace with function body.
--- npc_act_1/scripts/npc.gd
+++ npc_act_1/scripts/npc.gd
@@ -1,23 +1,24 @@
 extends CharacterBody2D
 
 @export var dialog : Array[DialogText]
+@export var trigger_id : String
 
 @onready var prompt_label = $Label
 
 func _ready() -> void:
-	if TriggersSystem.check_trigger("talked", true):
-		hide()
-		
+	if TriggersSystem.check_trigger(trigger_id, true):
+		queue_free()
+
 func _on_interactable_player_enter() -> void:
 	prompt_label.show()
-
 
 func _on_interactable_player_exit() -> void:
 	prompt_label.hide()
 
-
-
 func _on_interactable_interacted() -> void:
+	if TriggersSystem.check_trigger(trigger_id, true):
+		return
+	
 	EventSystem.cutscene_started.emit()
 	DialogueSystem.start_dialog(dialog)
-	TriggersSystem.update_trigger("talked", true)
+	TriggersSystem.update_trigger(trigger_id, true)
