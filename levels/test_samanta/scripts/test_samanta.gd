75c66d503315806ea287e09f127e172c184b21f4
Misaki <samantamancini17@gmail.com>
2024-11-12 22:29:00 +0100

    new global triggers for dialogues, sound telephone

--- levels/test_samanta/scripts/test_samanta.gd
+++ levels/test_samanta/scripts/test_samanta.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 func _ready() -> void:
 	EventSystem.transition_fade_out_end.connect(animation_end)
-	
+
 func animation_end():
 	LevelSystem.load_level("Test Simon")
 
 #func _on_area_2d_body_entered(body: Node2D) -> void:
 	#if body == %Player:
 		#TransictionScene.fade_out()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("debug_tel"):
+		TriggersSystem.update_trigger("ring", true)
--- telephone/script/telephone.gd
+++ telephone/script/telephone.gd
@@ -2,28 +2,55 @@ extends Node2D
 
 @onready var interact_prompt = $Label
 @onready var ring_sound = $Ring
-@export var dialog : Array[DialogText]
+@onready var hang_sound = $Hang
+@onready var collision_hide = $StaticBody2D/Interactable
 
-var sound = false
+@export var dialog_1 : Array[DialogText]
+@export var dialog_2 : Array[DialogText]
+@export var dialog_3 : Array[DialogText]
+@export var act : String
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sound = true
-	if sound:
+	if TriggersSystem.check_trigger("ring", true):
 		ring_sound.play()
-		
 		
 
 func _on_interactable_player_enter() -> void:
-	if sound:
+	if TriggersSystem.check_trigger("ring", true):
 		interact_prompt.show()
 
 func _on_interactable_player_exit() -> void:
-	if sound:
+	if TriggersSystem.check_trigger("ring", true):
 		interact_prompt.hide()
 
 func _on_interactable_interacted() -> void:
-	if sound:
+	if TriggersSystem.check_trigger("ring", true):
 		ring_sound.stop()
-		DialogueSystem.start_dialog(dialog)
-		sound = false
 		interact_prompt.hide()
+		collision_hide.hide()
+		EventSystem.cutscene_started.emit()
+		TriggersSystem.update_trigger("ring", false)
+		TriggersSystem.update_trigger(act, true)
+		start_dialog()
+
+func start_dialog():
+	hang_sound.play()
+	await get_tree().create_timer(1).timeout
+	if TriggersSystem.check_trigger("act_1", true):
+		DialogueSystem.start_dialog(dialog_1)
+		
+	if TriggersSystem.check_trigger("act_2", true):
+		DialogueSystem.start_dialog(dialog_2)
+		
+	if TriggersSystem.check_trigger("act_3", true):
+		DialogueSystem.start_dialog(dialog_3)
+
+
+func _on_timer_timeout() -> void:
+	if TriggersSystem.check_trigger("ring", true):
+		ring_sound.play()
+		$Timer.start()
+	else:
+		ring_sound.stop()
